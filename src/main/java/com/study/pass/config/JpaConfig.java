efcf73c933916b06370f9b14cd0eda8727e83ee3
wlsdks <wlsdks12@naver.com>
2023-07-08 11:00:56 +0900

    1. Jpa auditing 설정코드 추가

---
+++ src/main/java/com/study/pass/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.study.pass.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
