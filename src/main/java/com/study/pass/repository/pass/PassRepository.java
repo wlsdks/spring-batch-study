96bff748886b025ee0e728b924e46b65c76d1479
wlsdks <wlsdks12@naver.com>
2023-07-08 12:20:51 +0900

    1. pass의 repository추가

---
+++ src/main/java/com/study/pass/repository/pass/PassRepository.java
@@ -0,0 +1,6 @@
+package com.study.pass.repository.pass;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassRepository extends JpaRepository<PassEntity, Integer> {
+}
