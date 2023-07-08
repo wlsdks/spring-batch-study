1a291e7515742003dafb60cb293d42e1a1375f27
wlsdks <wlsdks12@naver.com>
2023-07-08 11:36:29 +0900

    1. 테스트 코드 추가 및 설정추가

--- src/main/java/com/study/pass/repository/packaze/PackageRepository.java
+++ src/main/java/com/study/pass/repository/packaze/PackageRepository.java
@@ -1,6 +1,23 @@
 package com.study.pass.repository.packaze;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+
+    @Transactional
+    @Modifying // 이걸 넣어줘야 한다. (데이터 변경이 되는 insert, update, delete 에서 사용된다.)
+    @Query(value = "UPDATE PackageEntity p " +
+                    "set p.count = :count, " +
+                    "p.period = :period " +
+                    "where p.packageSeq = :packageSeq")
+    int updateCountAndPeriod(Integer packageSeq, Integer count, Integer period);
+
 }
